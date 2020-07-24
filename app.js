1bf89b64ac23d5f21b9e5c14ea3f50dff9293cee
fkaraagac11 <fehmi11.qa@gmail.com>
2020-07-23 21:56:25 -0400

    DB Connection Code is added

--- app.js
+++ app.js
@@ -1,8 +1,66 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-    res.send("Stay Calm Project Server started");
+mongoose
+    .connect("mongodb://localhost:27017/staycalmDB", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log("Connected to DB!"))
+    .catch((error) => console.log(error.message));
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    city: String,
+    type: String,
+    roomNumber: Number,
 });
 
-app.listen(5555);
+const List = mongoose.model("List", listSchema);
+
+List.find({}, function (err, lists) {
+    if (err) {
+        console.log("OH NO ERROR!!!");
+        console.log(err);
+    } else {
+        console.log("ALL THE LISTS ....");
+        console.log(lists);
+    }
+});
+
+List.create(
+    {
+        name: "Blue House",
+        image:
+            "https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&h=350",
+        city: "McLean",
+        type: "Single House",
+        roomNumber: 3,
+    },
+    (err, lists) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(lists);
+        }
+    }
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/staycalm", (req, res) => {
+    res.send("StayCalm Project Server started");
+});
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
+
+app.listen(5555, () => {
+    console.log("TO STAY CALM YOU NEED nSTAYCALM");
+});
