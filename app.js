3965fd43ca0ce1863b1aed3ee4eebf7cfb284817
fkaraagac11 <fehmi11.qa@gmail.com>
2020-07-24 22:21:52 -0400

    DB Schema was added

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+//DB Connection
 mongoose
     .connect("mongodb://localhost:27017/staycalmDB", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useNewUrlParser: true,
     })
     .then(() => console.log("Connected to DB!"))
     .catch((error) => console.log(error.message));
 
+//SCHEMA SETUP
 const listSchema = new mongoose.Schema({
     name: String,
     image: String,
@@ -20,35 +21,8 @@ const listSchema = new mongoose.Schema({
     roomNumber: Number,
 });
 
+// SCHEMA compiled to module
 const List = mongoose.model("List", listSchema);
-
-List.find({}, function (err, lists) {
-    if (err) {
-        console.log("OH NO ERROR!!!");
-        console.log(err);
-    } else {
-        console.log("ALL THE LISTS ....");
-        console.log(lists);
-    }
-});
-
-List.create(
-    {
-        name: "Blue House",
-        image:
-            "https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&h=350",
-        city: "McLean",
-        type: "Single House",
-        roomNumber: 3,
-    },
-    (err, lists) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(lists);
-        }
-    }
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -61,6 +35,75 @@ app.get("/", (req, res) => {
     res.render("landing");
 });
 
-app.listen(5555, () => {
-    console.log("TO STAY CALM YOU NEED nSTAYCALM");
+app.get("/listings", (req, res) => {
+    //GET all lists from DB
+    List.find({}, (err, allLists) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("listings", { listings: allLists });
+        }
+    });
 });
+
+app.post("/listings", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    var address = req.body.address;
+    var address2 = req.body.address2;
+    var city = req.body.city;
+    var state = req.body.state;
+    var zip = req.body.zip;
+    var newListing = {
+        email: email,
+        password: password,
+        address: address,
+        address2: address2,
+        city: city,
+        state: state,
+        zip: zip,
+    };
+    List.create(newListing, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/listings");
+        }
+    });
+});
+
+app.get("/listings/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.listen(5555, () => {
+    console.log("TO STAY CALM YOU NEED STAYCALM");
+});
+
+// List.find({}, function (err, lists) {
+//     if (err) {
+//         console.log("OH NO ERROR!!!");
+//         console.log(err);
+//     } else {
+//         console.log("ALL THE LISTS ....");
+//         console.log(lists);
+//     }
+// });
+
+// List.create(
+//     {
+//         name: "Blue House",
+//         image:
+//             "https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&h=350",
+//         city: "McLean",
+//         type: "Single House",
+//         roomNumber: 3,
+//     },
+//     (err, lists) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(lists);
+//         }
+//     }
+// );
