b2be115db1eed75808c9d79c6210882ebe9637de
fkaraagac11 <fehmi11.qa@gmail.com>
2020-11-06 23:01:27 -0500

    update

---
+++ Client/src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./header.css"
+
+export default function header() {
+    return (
+        <div >
+            <div style={{fontSize:30, color:"red"}}>Hello World</div>
+            <p>StayCalm</p>
+            <p>Sign-On</p>
+            <p>Log-In</p>
+        </div>
+    )
+
+
+    
+}
+
+
+  
+  
