19eeb0b4afc7901bde296926d432649e87c0a065
fkaraagac11 <fehmi11.qa@gmail.com>
2020-11-08 23:22:23 -0500

    add

---
+++ Client/src/components/Header copy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css"
+
+
+export default function Header() {
+    return (
+        <div className="header-nav" >
+            <p>StayCalm</p>
+            <p>Sign-On</p>
+            <p>Log-In</p>
+        </div>
+    )
+    
+}
+
+
+
+
+
+ 
