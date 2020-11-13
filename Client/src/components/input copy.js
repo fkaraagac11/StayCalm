4433fb5eccf961357a7e71ba037ead6f1c0cd47b
fkaraagac11 <fehmi11.qa@gmail.com>
2020-11-12 23:39:25 -0500

    add

---
+++ Client/src/components/input copy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function input() {
+    return (
+        <div>
+            <label>Search Box</label>
+            <input type="text" />
+            <input type="email" />
+        </div>
+    )
+}
